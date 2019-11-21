73c5f1544beced09ca698edc489385b4774bf0ef
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-21 14:16:38 -0600

    completed next exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (long i = 2L; i < 1000000; i *= i) {
-            System.out.println(i);
+        for (int i = 1; i < 100; i ++) {
+            if (i % 15 == 0) {
+                System.out.println("fizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
