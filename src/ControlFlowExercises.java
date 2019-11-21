cd47f4b937bb61ecf7ea4f38897c59d04d5f46d8
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-21 15:10:53 -0600

    completed control flow exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class ControlFlowExercises {
-    public static void main (String[] args) {
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("enter a number of your choosing");
-        long userInput = scanner.nextInt();
-
-        System.out.print("number | squared | cubed \n");
-        System.out.print("-------|---------|-------\n");
-        for (long i = 1; i < userInput; i++) {
-            System.out.print(i + "      |    " + i*i + "     |    " + i*i*i +"\n");
+        System.out.println("Please enter a number between 0 and 100");
+        byte userNum = scanner.nextByte();
+        if (userNum > 88) {
+            System.out.println("You got an 'A'!");
+        } else if (userNum >= 80) {
+            System.out.println("You got a B");
+        } else if (userNum > 67) {
+            System.out.println("You got a C");
+        } else if (userNum > 60) {
+            System.out.println("You got a D");
+        } else {
+            System.out.println("You got an F.");
         }
     }
 }
