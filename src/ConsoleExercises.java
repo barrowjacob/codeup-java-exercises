be6449aa605a8f2a70835e290b5373998876f99d
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-21 11:36:46 -0600

    completed consoleExercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately: %.2f",  pi);
+//    }
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("@");
+
+
+//        System.out.print("Enter an integer: ");
+//        Integer userNum = scanner.nextInt();
+//        System.out.println("You entered the number --> " + userNum);
+
+        System.out.println("Enter three word: ");
+        String firstWord = scanner.next();
+        String secondWord = scanner.next();
+        String thirdWord = scanner.next();
+
+        System.out.print(firstWord + "\n");
+        System.out.print(secondWord + "\n");
+        System.out.print(thirdWord + "\n");
+//
+
+        scanner.nextLine();
+//
+//        System.out.print("Write a sentence, please.");
+//        String thisSentence = scanner.nextLine();
+//        System.out.print(thisSentence);
+
+//
+//        System.out.print("Enter length, width and height");
+//        String classLength = scanner.next();
+//        String classWidth = scanner.next();
+//        String classHeight = scanner.next();
+//        float newLength = Float.parseFloat(classLength);
+//        float newWidth = Float.parseFloat(classWidth);
+//        float newHeight = Float.parseFloat(classHeight);
+//        System.out.print(2* newLength + 2* newWidth + "\n");
+//        System.out.print(newLength * newWidth + "\n");
+//        System.out.print(newLength * newWidth * newHeight + "\n");
+
+
+
+
+    }
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,30 +1,29 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello");
-        System.out.print(" World!");
-        int myFavoriteNumber = 27;
-        System.out.print("\n" + myFavoriteNumber);
-        String myString = "potatoes";
-        System.out.print(myString);
-        float myNumber = 3.14f;
-        System.out.print(myNumber);
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
-        int x = 4;
-        x += 5;
-        System.out.print(x);
-        int x = 3;
-        int y = 4;
-        y *= x;
-        System.out.print(y);
-        int x = 10;
-        int y = 2;
-        x /= y;
-        y -= x;
-        System.out.print(x);
-        System.out.print(y);
+//        System.out.print("Hello");
+//        System.out.print(" World!");
+//        int myFavoriteNumber = 27;
+//        System.out.print("\n" + myFavoriteNumber);
+//        String myString = "potatoes";
+//        System.out.print(myString);
+//        float myNumber = 3.14f;
+//        System.out.print(myNumber);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        System.out.print(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.print(y);
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.print(x);
+//        System.out.print(y);
     }
-
 }
 
