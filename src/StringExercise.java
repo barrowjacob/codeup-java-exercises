8f77f08b9bab7176f35054a6e61bf473d3772a56
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-22 09:53:01 -0600

    completed first string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String education = "We don't need no education \n We don't need no thought control";
+        System.out.println(education);
+        String quotes = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quotes);
+        String windows = "In Windows, the main drive is usually C:\\";
+        System.out.println(windows);
+        String backslashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backslashes);
+
+    }
+}
