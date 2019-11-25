ebd33f9863c7c3f6fca38d69f5e3a4a6addc1c40
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-25 06:24:31 -0600

    added turtlebear ascii art

--- src/sandbox.java
+++ src/sandbox.java
@@ -4,16 +4,31 @@ public class sandbox {
 
     public static void main(String[] args) {
 
-           Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-//           //create a program that outputs 10 numbers entered into console
-           System.out.println("Please enter 10 numbers");
-              int totalNums = 0;
-              for (int i = 0; i < 10; i++) {
-                     Integer n = scanner.nextInt();
-                     totalNums += n;
-              }
+////           //create a program that outputs 10 numbers entered into console
+//           System.out.println("Please enter 10 numbers");
+//              int totalNums = 0;
+//              for (int i = 0; i < 10; i++) {
+//                     Integer n = scanner.nextInt();
+//                     totalNums += n;
+//              }
+//
+//           System.out.println("Your grand total is: " + totalNums);
+//    }
+//}
 
-           System.out.println("Your grand total is: " + totalNums);
+        System.out.println("  ____  _                   __   _____ _          \n" +
+                " |  _ \\(_)___  ___    ___  / _| |_   _| |__   ___ \n" +
+                " | |_) | / __|/ _ \\  / _ \\| |_    | | | '_ \\ / _ \\\n" +
+                " |  _ <| \\__ \\  __/ | (_) |  _|   | | | | | |  __/\n" +
+                " |_| \\_\\_|___/\\___|  \\___/|_|     |_| |_| |_|\\___|\n" +
+                "                                                  ");
+        System.out.println(" _____ _   _______ _____ _      _____  ______ _____  ___  ______ \n" +
+                "|_   _| | | | ___ \\_   _| |    |  ___| | ___ \\  ___|/ _ \\ | ___ \\\n" +
+                "  | | | | | | |_/ / | | | |    | |__   | |_/ / |__ / /_\\ \\| |_/ /\n" +
+                "  | | | | | |    /  | | | |    |  __|  | ___ \\  __||  _  ||    / \n" +
+                "  | | | |_| | |\\ \\  | | | |____| |___  | |_/ / |___| | | || |\\ \\ \n" +
+                "  \\_/  \\___/\\_| \\_| \\_/ \\_____/\\____/  \\____/\\____/\\_| |_/\\_| \\_|");
     }
 }
