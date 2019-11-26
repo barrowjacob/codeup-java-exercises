2dc8df643080a5b07a3eb971f0696a08c0ee9a02
Jacob Barrow <jacob.barrow@gmail.com>
2019-11-26 15:07:37 -0600

    completed exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -5,6 +5,8 @@ public class CircleApp {
     public static void main(String[] args) {
         Input input = new Input();
         Circle circle = new Circle(input.getDouble());
-        System.out.println("");
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
+
     }
 }
