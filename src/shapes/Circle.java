ef9cd7b43175866fac5c3f9e0195c71ba5480962
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-02 06:46:17 -0600

    adjusted input.java

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -2,16 +2,15 @@ package shapes;
 
 class Circle {
     private static double radius;
+
     public Circle(double radius) {
         this.radius = radius;
     }
     public double getArea() {
-        double area = Math.PI * (radius * radius);
-        return area;
+        return Math.PI * (radius * radius);
     }
     public double getCircumference() {
-        double circumference = (2 * Math.PI * radius);
-        return circumference;
+        return (2 * Math.PI * radius);
     }
     public static void main(String[] args) {
         Circle circle = new Circle(1.0);
--- src/util/Input.java
+++ src/util/Input.java
@@ -7,57 +7,42 @@ public class Input {
         public String getString() {
             return scanner.next();
         }
+
         boolean yesNo() {
-            String newYes = scanner.next();
+            System.out.println("Please enter \"yes/y\"");
+            String newYes = getString();
             return newYes.equalsIgnoreCase("yes") || newYes.equalsIgnoreCase("y");
         }
+
         public int getInt(int min, int max) {
-                System.out.printf("Please enter a number between %s and %s", min, max);
-                boolean sandwich = true;
-                int newInput = scanner.nextInt();
-                while(sandwich) {
-                    if (newInput > min && newInput < max) {
-                        System.out.println("YOU DID IT!");
-                        sandwich = false;
-                        return newInput;
-                    } else if (newInput > max) {
-                        System.out.printf("Your guess was %s. Please guess LOWER (between 1 and 10).", newInput);
-                    } else {
-                        System.out.printf("Your guess was %s. Please guess HIGHER (between 1 and 10).", newInput);
-                    } newInput = scanner.nextInt();
-            } return newInput;
+            System.out.printf("Please enter number between %s and %s", min, max);
+            int number = getInt();
+
+            if (number < min)
+                return getInt(min, max);
+            else if (number > max)
+                return getInt(min, max);
+            return number;
         }
+
         public int getInt() {
             System.out.printf("enter number");
-            int userInputs = scanner.nextInt();
-            System.out.println(userInputs);
-            return userInputs;
+            return Integer.parseInt(getString());
         }
 
-    public double getDouble(double minny, double maxy) {
-        System.out.printf("Please enter a number between %s and %s", minny, maxy);
-        boolean sandwiches = true;
-        double newInputs = scanner.nextDouble();
-        while(sandwiches) {
-            if (newInputs > minny && newInputs < maxy) {
-                System.out.println("YOU DID IT!");
-                sandwiches = false;
-                return newInputs;
-            } else if (newInputs > maxy) {
-                System.out.printf("Your guess was %s. Please guess LOWER (between 1 and 10).", newInputs);
-            } else {
-                System.out.printf("Your guess was %s. Please guess HIGHER (between 1 and 10).", newInputs);
-            } newInputs = scanner.nextDouble();
-        } return newInputs;
-    }
-    public double getDouble(){
-        System.out.println("enter number");
-            double doubleInput = scanner.nextDouble();
-        System.out.println(doubleInput);
-        return doubleInput;
-    };
+        public double getDouble(int min, int max) {
+            double number = getDouble();
+            if (number < min)
+                return getDouble(min, max);
+            else if (number > max)
+                return getDouble(min, max);
+            return number;
+        }
 
-
+        public double getDouble(){
+            System.out.println("enter number");
+           return Integer.parseInt(getString());
+        }
 }
 
 
