3c5f913a31ec55be9d083c4b583326fe20e93f60
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-03 14:34:16 -0600

    corrected side error

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,12 +1,12 @@
 package shapes;
 
 public class Square extends Rectangle{
-    int side;
 //use 'super' to reference super class when you have default constructor issues
     public Square(int side) {
         //parameters passed through have to include super class parameters that you want to reference
         super(side, side);
-        this.side = side;
+        this.length = side;
+        this.width = side;
     }
     @Override
     public double getArea() {
