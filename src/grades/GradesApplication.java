2ba7f3df7e33e440ba13803cc15bf8b15d62e6c4
Jacob Barrow <jacob.barrow@gmail.com>
2019-12-05 11:08:01 -0600

    completed main arrayList exercise

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,56 @@
+package grades;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class GradesApplication {
+
+
+
+    public static void main(String[] args) {
+
+        Student john = new Student("john");
+        john.addGrade(45);
+        john.addGrade(55);
+        john.addGrade(65);
+
+        Student jahn = new Student("jahn");
+        jahn.addGrade(45);
+        jahn.addGrade(45);
+        jahn.addGrade(55);
+
+        Student jehn = new Student("jehn");
+        jehn.addGrade(45);
+        jehn.addGrade(95);
+        jehn.addGrade(35);
+
+        Student jihn = new Student("jihn");
+        jihn.addGrade(45);
+        jihn.addGrade(25);
+        jihn.addGrade(65);
+
+
+        HashMap<String, Student> usernames = new HashMap<>();
+        usernames.put("flamingBird123", john);
+        usernames.put("flamingBird223", jahn);
+        usernames.put("flamingBird323", jehn);
+        usernames.put("flamingBird423", jihn);
+
+        while (true) {
+            System.out.println("Which student would you like to see more info about: " + usernames.keySet());
+            Scanner sc = new Scanner(System.in);
+            String userInput = sc.nextLine();
+            if (usernames.get(userInput) != null) {
+                System.out.printf("Name: %s - Github Username: %s \nCurrent GPA: %.2f", usernames.get(userInput).getName(), userInput, usernames.get(userInput).getAverageGrade());
+                System.out.println("Would you like to see another student?");
+                userInput = sc.nextLine();
+                if (userInput.equalsIgnoreCase("n") || userInput.equalsIgnoreCase("no")) {
+                    break;
+                }
+            } else {
+                System.out.println("That username does not exist.");
+            }
+        }
+    }
+
+}
---
+++ src/grades/Student.java
@@ -0,0 +1,36 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name) {
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+    public float getAverageGrade(){
+        float newTotal = 0;
+        for (float grade : this.grades){
+            newTotal += grade;
+        } return newTotal / this.grades.size();
+    }
+    public static void main(String[] args) {
+        Student james = new Student("james");
+        james.addGrade(45);
+        james.addGrade(80);
+        james.addGrade(65);
+        System.out.println(james.getAverageGrade());
+    }
+
+
+}
